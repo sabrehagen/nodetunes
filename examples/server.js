4277c7a82cc1d0f20affc3207ca302f92543d155
Jim Lake <jim@blueskylabs.com>
2015-12-16 16:06:49 -0800

    move decoding out of the core

--- examples/server.js
+++ examples/server.js
@@ -1,17 +1,62 @@
 'use strict';
 
-var AirTunesServer = require('../index');
-var Speaker = require('speaker');
+const AirTunesServer = require('../index');
+const Speaker = require('speaker');
+const AlacDecoderStream = require('alac2pcm');
 
-var speaker = new Speaker({
+const speaker = new Speaker({
   channels: 2,
   bitDepth: 16,
   sampleRate: 44100,
 });
-var server = new AirTunesServer({ serverName: 'NodeTunes Example' });
+const server = new AirTunesServer({ serverName: 'NodeTunes Example' });
 
-server.on('clientConnected',(stream) => {
-  stream.pipe(speaker);
+let g_codec = '96 L16/44100/2';
+
+let g_decoder = null;
+
+server.on('clientConnected',(args) => {
+  console.log("audioCodec:",args.audioCodec);
+  g_codec = args.audioCodec;
+
+  if (g_codec == '96 AppleLossless') {
+    const audioOptions = args.audioOptions;
+    const decoderOptions = {
+      frameLength: parseInt(audioOptions[1], 10),
+      compatibleVersion: parseInt(audioOptions[2], 10),
+      bitDepth: parseInt(audioOptions[3], 10),
+      pb: parseInt(audioOptions[4], 10),
+      mb: parseInt(audioOptions[5], 10),
+      kb: parseInt(audioOptions[6], 10),
+      channels: parseInt(audioOptions[7], 10),
+      maxRun: parseInt(audioOptions[8], 10),
+      maxFrameBytes: parseInt(audioOptions[9], 10),
+      avgBitRate: parseInt(audioOptions[10], 10),
+      sampleRate: parseInt(audioOptions[11], 10)
+    };
+
+    g_decoder = new AlacDecoderStream(decoderOptions);
+  }
+});
+
+server.on('audio',(audio,sequence_num,rtp_ts) => {
+  if (g_codec == '96 L16/44100/2') {
+    for (let i = 0 ; i < audio.length ; i+=2) {
+      const temp = audio[i];
+      audio[i] = audio[i + 1];
+      audio[i + 1] = temp;
+    }
+    speaker.write(audio);
+  } else if (g_codec == '96 AppleLossless') {
+    g_decoder.write(audio);
+    let buf = g_decoder.read();
+    while (buf != null) {
+      speaker.write(buf);
+      buf = g_decoder.read();
+    }
+  } else {
+    console.log("unsupported codec:",g_codec);
+  }
 });
 
 server.on('volumeChange',(volume) => {
@@ -32,6 +77,10 @@ server.on('teardown',() => {
 
 server.on('metadataChange',(metadata) => {
   console.log("metadataChange:",metadata);
+});
+
+server.on('error',(args) => {
+  console.error("error:",args);
 });
 
 server.start();
